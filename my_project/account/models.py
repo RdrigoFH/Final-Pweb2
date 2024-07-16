21dfba7144cc3ad8506e5d90f472644ed1e1ed95
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-16 18:37:20 -0500

    creando el modelo PayModel  que manejará la información del procesamiento de pagos

--- my_project/account/models.py
+++ my_project/account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class PayModel(models.Model):
+    email = models.EmailField(null=True, blank=True)
+    name_on_card = models.CharField(max_length=200, null=True, blank=True)
+    customer_id = models.CharField(max_length=200, blank=True, null=True)
+    card_number = models.CharField(max_length=16, unique=True, null=True, blank=True)
+    exp_month = models.CharField(max_length=2, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
+    exp_year = models.CharField(max_length=4, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
+    card_id = models.TextField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(User, related_name="stripemodel", on_delete=models.CASCADE, null=True, blank=True)
+    address_city = models.CharField(max_length=120, null=True, blank=True)
+    address_country = models.CharField(max_length=120, null=True, blank=True)
+    address_state = models.CharField(max_length=120, null=True, blank=True)
+    address_zip = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
+
+    def __str__(self):
+        return self.email
