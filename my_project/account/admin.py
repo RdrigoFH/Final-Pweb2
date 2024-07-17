2affdbb61c4a5cb78c6143af47bc7e998f57160c
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-16 23:38:45 -0500

    se completa el contenido de admin.py para registrar y personalizar la visualización de los modelos en el panel de administración de Django.

--- my_project/account/admin.py
+++ my_project/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import PayModel, BillingAddress, OrderModel
+
+class PayModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "card_number", "user", "exp_month", "exp_year", "customer_id", "card_id")
+
+class BillingAddressModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "user", "phone_number", "pin_code", "house_no", "landmark", "city", "state")
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "card_number", "address", "ordered_item", "paid_status", "paid_at", "total_price", "is_delivered", "delivered_at", "user")
+
+admin.site.register(PayModel, PayModelAdmin)
+admin.site.register(BillingAddress, BillingAddressModelAdmin)
+admin.site.register(OrderModel, OrderModelAdmin)
