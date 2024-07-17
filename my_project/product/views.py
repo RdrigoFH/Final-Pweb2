69ff227a993d9f4d26b1fc454c2f0c93cf93845f
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-17 17:42:44 -0500

    Se manejan las solicitudes post para crear nuevos productos

--- my_project/product/views.py
+++ my_project/product/views.py
@@ -14,6 +14,28 @@ class ProductDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class ProductCreateView(APIView):
+
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request):
+        user = request.user
+        data = request.data
+
+        product = {
+            "name": data["name"],
+            "description": data["description"],
+            "price": data["price"],
+            "stock": data["stock"],
+            "image": data["image"],
+        }
+
+        serializer = ProductSerializer(data=product, many=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
