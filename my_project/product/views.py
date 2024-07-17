1ce9e0a1b54d7f13cfe5ff3cabdc8478fdb9a63f
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-17 17:46:46 -0500

    Se hacen las importaciones necesarios

--- my_project/product/views.py
+++ my_project/product/views.py
@@ -1,3 +1,13 @@
+from .models import Product
+from rest_framework import status
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework.decorators import permission_classes
+
+
 class ProductView(APIView):
 
     def get(self, request):
