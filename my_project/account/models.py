ef651979ddcf5e66edfafadfa73162688dd73364
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-16 22:44:29 -0500

    creando el modelo BillingAddress que manejará las direcciones de facturación de los usuarios

--- my_project/account/models.py
+++ my_project/account/models.py
@@ -18,3 +18,15 @@ class PayModel(models.Model):
 
     def __str__(self):
         return self.email
+class BillingAddress(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    user = models.ForeignKey(User, related_name="billingmodel", on_delete=models.CASCADE, null=True, blank=True)
+    phone_number = models.CharField(max_length=10, validators=[RegexValidator(r'^\+?1?\d{9,15}$')], null=False, blank=False)
+    pin_code = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{0,9}$')], null=False, blank=False)
+    house_no = models.CharField(max_length=300, null=False, blank=False)
+    landmark = models.CharField(max_length=120, null=False, blank=False)
+    city = models.CharField(max_length=120, null=False, blank=False)
+    state = models.CharField(max_length=120, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
