432a17af8a42486e08e717843e296460b3fd045d
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 18:14:19 -0500

    craando el reductor que maneja las acciones relacionadas con la lista de tarjetas guardadas

--- frontend/src/reducers/cardReducers.js
+++ frontend/src/reducers/cardReducers.js
@@ -97,4 +97,32 @@ export const chargeCardReducer = (state = {}, action) => {
             return state
     }
 }
+export const savedCardsListReducer = (state = {stripeCards: []}, action) => {
+    switch (action.type) {
+        case SAVED_CARDS_LIST_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                success: false,
+                error: ""
+            }
+        case SAVED_CARDS_LIST_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                success: true,
+                stripeCards: action.payload,
+                error: ""
+            }
+        case SAVED_CARDS_LIST_FAIL:
+            return {
+                ...state,
+                loading: false,
+                success: false,
+                error: action.payload
+            }
+        default:
+            return state
+    }
+}
 
