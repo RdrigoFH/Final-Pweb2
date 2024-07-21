01ca9b6836e8b1c55e9a3e4ffed412a4b43dd4c4
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-21 14:40:23 -0500

    Se hace el cierre de sesion, el registro de nuevo clientey la verificacion del token valido en stripe

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -73,3 +73,81 @@ export const login = (username, password) => async (dispatch) => {
        })
    }
 }
+
+export const logout = () => (dispatch) => {
+   localStorage.removeItem('userInfo')
+   dispatch({
+       type: USER_LOGOUT
+   })
+   dispatch({
+       type: CARD_CREATE_RESET
+   })
+}
+
+export const register = (username, email, password) => async (dispatch) => {
+   try {
+       dispatch({ type: USER_REGISTER_REQUEST })
+
+       const config = {
+           headers: {
+               'Content-type': 'application/json'
+           }
+       }
+
+       const { data } = await axios.post(`/account/register/`,
+           { 'username': username, 'email': email, 'password': password },
+           config
+       )
+
+       dispatch({
+           type: USER_REGISTER_SUCCESS,
+           payload: data
+       })
+
+       dispatch({
+           type: USER_LOGIN_SUCCESS,
+           payload: data
+       })
+
+       localStorage.setItem('userInfo', JSON.stringify(data))
+   }
+   catch (error) {
+       dispatch({
+           type: USER_REGISTER_FAIL,
+           payload: error.response && error.response.data.detail ? error.response.data.detail : error.message
+       })
+   }
+}
+
+export const checkTokenValidation = () => async (dispatch, getState) => {
+   try {
+
+       dispatch({
+           type: CHECK_TOKEN_VALID_REQUEST
+       })
+
+       const {
+           userLoginReducer: { userInfo }
+       } = getState()
+
+       const config = {
+           headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${userInfo.token}`
+           }
+       }
+
+       const { data } = await axios.get("/payments/check-token/", config)
+
+       dispatch({
+           type: CHECK_TOKEN_VALID_SUCCESS,
+           payload: data
+       })
+
+   } catch (error) {
+       dispatch({
+           type: CHECK_TOKEN_VALID_FAIL,
+           payload: error.response && error.response.data.details ? error.response.data.details : error.message
+       })
+   }
+}
