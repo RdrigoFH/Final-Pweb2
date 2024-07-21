e1b19bf663c00fe292cc87cc4179cb528399c653
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-21 14:17:33 -0500

    Se obtienen los detalles de productos mediante el id de producto

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -47,3 +47,22 @@ export const getProductsList = () => async (dispatch) => {
         })
     }
 }
+export const getProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({
+            type: PRODUCT_DETAILS_REQUEST
+        })
+
+        const { data } = await axios.get(`/api/product/${id}/`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.message
+        })
+    }
+}
