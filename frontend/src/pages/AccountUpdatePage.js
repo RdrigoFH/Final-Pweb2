7fa0739535c1ebd23d481307f2560556d74eaa2d
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 18:12:30 -0500

    Proporcionando la logica para renderizar el formulario

--- frontend/src/pages/AccountUpdatePage.js
+++ frontend/src/pages/AccountUpdatePage.js
@@ -66,6 +66,90 @@ function AccountUpdatePage() {
         history.push("/account/")
         dispatch(userDetails(userInfo.id))
     }    
+
+    const renderData = () => {
+        try {
+            return (
+                <div>
+                    <Row className='justify-content-md-center'>
+                        <Col xs={12} md={6}>
+                            <span
+                                className="d-flex justify-content-center"
+                                style={{ display: "flex", marginBottom: "15px", color: "#008080" }}>
+                                <em>Update User Details</em>
+                            </span>
+                            {loading && <Spinner animation="border" />}
+                            <Form onSubmit={onSubmit}>
+
+                                <Form.Group controlId='username'>
+                                    <Form.Label>
+                                        Username
+                                    </Form.Label>
+                                    <Form.Control
+                                        autoFocus={true}
+                                        type="text"
+                                        defaultValue={userAccDetails.username}
+                                        placeholder="username"
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    >
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group controlId='email'>
+                                    <Form.Label>
+                                        Email Address
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="enter email"
+                                        defaultValue={userAccDetails.email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    >
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group controlId='password'>
+                                    <Form.Label>
+                                        Reset-Password
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="enter new password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    >
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group controlId='confirmPassword'>
+                                    <Form.Label>
+                                        Confirm Password
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="confirm new password"
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                    >
+                                    </Form.Control>
+                                </Form.Group>
+
+                                <Button type="submit" variant='success' className="btn-sm">Save Changes</Button>
+                                <Link to={`/account`}>
+                                    <button className="btn btn-primary btn-sm ml-2" type="button">
+                                        Cancel
+                                    </button>
+                                </Link>
+                            </Form>
+                        </Col>
+                    </Row>
+                </div>
+            )
+        } catch (error) {
+            return <Message variant='danger'>Something went wrong, go back to <Link
+                onClick={logoutHandler} to={`/login`}
+            > Login</Link> page.</Message>
+        }
+    }
+    return  renderData()
 }
 
 export default AccountUpdatePage
