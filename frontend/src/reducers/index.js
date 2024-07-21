dd549fa694cef394269ae6202cac558d32bffa53
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 18:42:00 -0500

    se crea index.js e importaciones para  combinar múltiples reductores en uno solo

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,32 @@
+import { combineReducers } from "redux";
+import {
+    productsListReducer,
+    productDetailsReducer,
+    createProductReducer,
+    updateProductReducer,
+    deleteProductReducer,
+    changeDeliveryStatusReducer,
+} from "./productReducers";
+
+import {
+    createCardReducer,
+    chargeCardReducer,
+    savedCardsListReducer,
+    deleteSavedCardReducer,
+    updateStripeCardtReducer
+} from "./cardReducers";
+
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userDetailsUpdateReducer,
+    deleteUserAccountReducer,
+    checkTokenValidationReducer,
+    getSingleAddressReducer,
+    getAllAddressesOfUserReducer,
+    createUserAddressReducer,
+    updateUserAddressReducer,
+    deleteUserAddressReducer,
+    getAllOrdersReducer,
+} from "./userReducers";
