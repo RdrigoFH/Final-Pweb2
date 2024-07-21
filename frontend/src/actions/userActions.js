db66ab130b4e0730429bb5a83e88d123661dd266
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-21 14:41:17 -0500

    Informacion de un cliente por su primary key Id

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -151,3 +151,36 @@ export const checkTokenValidation = () => async (dispatch, getState) => {
        })
    }
 }
+
+export const userDetails = (id) => async (dispatch, getState) => {
+
+   try {
+
+       dispatch({
+           type: USER_DETAILS_REQUEST
+       })
+
+       const {
+           userLoginReducer: { userInfo }
+       } = getState()
+
+       const config = {
+           headers: {
+               "Content-Type": "application/json",
+               Authorization: `Bearer ${userInfo.token}`
+           }
+       }
+       const { data } = await axios.get(`/account/user/${id}`, config)
+
+       dispatch({
+           type: USER_DETAILS_SUCCESS,
+           payload: data
+       })
+
+   } catch (error) {
+       dispatch({
+           type: USER_DETAILS_FAIL,
+           payload: error.response && error.response.data.details ? error.response.data.details : error.message
+       })
+   }
+}
