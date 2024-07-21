39115f4e8d04846e17ae5f485753235de39f0ea2
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 17:42:13 -0500

    creando la carpeta de reductores y el archivo cardReducers.js para manejar ciertas operaciones relacionadas con tarjetas de pago

--- my_project/account/admin.py
+++ my_project/account/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import StripeModel, BillingAddress, OrderModel
 
-class PayModelAdmin(admin.ModelAdmin):
+class StripeModelAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "card_number", "user", "exp_month", "exp_year", "customer_id", "card_id")
 
 class BillingAddressModelAdmin(admin.ModelAdmin):
@@ -13,6 +13,6 @@ class BillingAddressModelAdmin(admin.ModelAdmin):
 class OrderModelAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "card_number", "address", "ordered_item", "paid_status", "paid_at", "total_price", "is_delivered", "delivered_at", "user")
 
-admin.site.register(StripeModel, PayModelAdmin)
+admin.site.register(StripeModel, StripeModelAdmin)
 admin.site.register(BillingAddress, BillingAddressModelAdmin)
 admin.site.register(OrderModel, OrderModelAdmin)
