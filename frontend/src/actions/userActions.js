abe9c8f8ceb8bd7f3ab7c985b8a598880d69e135
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-21 14:39:13 -0500

    se maneja el envio de la solicitud para el inicio de sesion  y almacena la infformacion del usuario

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -41,3 +41,35 @@ import {
 } from '../constants/index'
 
 import axios from 'axios'
+export const login = (username, password) => async (dispatch) => {
+   try {
+       dispatch({
+           type: USER_LOGIN_REQUEST
+       })
+
+       const config = {
+           headers: {
+               'Content-type': 'application/json'
+           }
+       }
+
+       const { data } = await axios.post(
+           '/account/login/',
+           { 'username': username, 'password': password },
+           config
+       )
+
+       dispatch({
+           type: USER_LOGIN_SUCCESS,
+           payload: data
+       })
+
+       localStorage.setItem('userInfo', JSON.stringify(data)) 
+      
+   } catch (error) {
+       dispatch({
+           type: USER_LOGIN_FAIL,
+           payload: error.response && error.response.data.detail ? error.response.data.detail : error.message
+       })
+   }
+}
