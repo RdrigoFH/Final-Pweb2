ecedd4b3cf522edc57d89f5605afb1c23f835816
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 20:12:38 -0500

    Renderizado del componente el cual mostrara los detalles del producto y un modal de confirmacion de eliminacion del producto

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -49,6 +49,106 @@ function ProductDetailsPage({ history, match }) {
             type: DELETE_PRODUCT_RESET
         })
     }
+
+    return (
+        <div>
+
+            {/* Modal Start*/}
+            <div>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            <i style={{ color: "#e6e600" }} className="fas fa-exclamation-triangle"></i>
+                            {" "}
+                            Delete Confirmation
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Are you sure you want to delete this product <em>"{product.name}"</em>?</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={() => confirmDelete()}>
+                            Confirm Delete
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+
+            {/* Modal End */}
+
+            {loading && <span style={{ display: "flex" }}>
+                <h5>Getting Product Details</h5>
+                <span className="ml-2">
+                    <Spinner animation="border" />
+                </span>
+            </span>}
+            {error ? <Message variant='danger'>{error}</Message>
+                :
+                <div>
+                    <Container>
+                        <Row>
+                            <Col md={6}>
+                                <Card.Img variant="top" src={product.image} height="420" />
+
+                                {/* Product edit and delete conditions */}
+
+                                {userInfo && userInfo.admin ?
+                                    <span style={{ display: "flex" }}>
+                                        < button
+                                            className="btn mt-2 btn-danger btn-sm button-focus-css"
+                                            style={{ width: "100%" }}
+                                            onClick={() => handleShow()}
+                                        >Delete Product
+                                        </button>
+
+                                        <button
+                                            className="ml-2 mt-2 btn btn-primary btn-sm button-focus-css"
+                                            onClick={() => history.push(`/product-update/${product.id}/`)}
+                                            style={{ width: "100%" }}
+                                        >Edit Product
+                                        </button>
+                                    </span>
+                                    : ""}
+                            </Col>
+
+                            <Col sm>
+                                <b>{product.name}</b>
+                                <hr />
+                                <span className="justify-description-css">
+                                    <p>{product.description}</p>
+                                </span>
+                                <span style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    border: "1px solid",
+                                    borderColor: "#C6ACE7",
+                                    padding: "2px"
+                                }}>
+                                    Price:<span className="text-success ml-2">₹ {product.price}</span>
+                                </span>
+                            </Col>
+                            <Col sm>
+                                <b>Buy</b>
+                                <hr />
+                                {product.stock ?
+                                    <Link to={`${product.id}/checkout/`}>
+                                        <button className="btn btn-primary">
+                                            <span>Pay with Stripe</span>
+                                        </button>
+                                    </Link>
+                                    :
+                                    <Message variant='danger'>
+                                        Out Of Stock!
+                                    </Message>}
+                            </Col>
+                        </Row>
+
+                    </Container>
+                </div>
+            }
+        </div >
+    )
 }
 
 export default ProductDetailsPage
