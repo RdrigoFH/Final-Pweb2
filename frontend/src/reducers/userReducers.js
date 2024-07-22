c130d1d710eee9e2f4de8f9970a06ea39e6e2a2e
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 20:07:35 -0500

    creando reductor que maneja el estado de obtener todas las órdenes del usuario.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -412,3 +412,31 @@ export const deleteUserAddressReducer = (state = {}, action) => {
             return state
     }
 }
+
+export const getAllOrdersReducer = (state = {orders: []}, action) => {
+    switch(action.type) {
+        case GET_ALL_ORDERS_REQUEST:
+            return {
+                ...state, 
+                loading: true,
+                orders: [],
+                error: ""
+            }
+        case GET_ALL_ORDERS_SUCCESS:
+            return {
+                ...state, 
+                loading: false,
+                orders: action.payload,
+                error: ""
+            }
+        case GET_ALL_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                orders: [],
+                error: action.payload
+            }
+        default:
+            return state
+    }
+}
