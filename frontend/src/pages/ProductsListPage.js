74f9974be239bff741c5d2dfb99e218fff445af9
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 20:22:00 -0500

    Agregando el renderizado del componente ProductsListPage el cual filtrara los productos basandose en la url, si no encuentra producto se mostrar un mensaje showNothingMessage

--- frontend/src/pages/ProductsListPage.js
+++ frontend/src/pages/ProductsListPage.js
@@ -31,6 +31,39 @@ function ProductsListPage() {
             </div>
         )
     }
+
+    return (
+        <div>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <span style={{ display: "flex" }}>
+                <h5>Getting Products</h5>
+                <span className="ml-2">
+                    <Spinner animation="border" />
+                </span>
+            </span>}
+            <div>
+                <Row>
+
+                    {/* Si la longitud del resultado del filtro es igual a 0, se mostrará el mensaje "no se encontró nada".
+                        con la ayuda de la función showNothingMessage; de ​​lo contrario, muestra el resultado filtrado en el
+                        página web y luego ejecutar la función de mapa */}
+
+                    {(products.filter((item) =>
+                        item.name.toLowerCase().includes(searchTerm !== "" ? searchTerm.split("=")[1] : "")
+                    )).length === 0 ? showNothingMessage() : (products.filter((item) =>
+                        item.name.toLowerCase().includes(searchTerm !== "" ? searchTerm.split("=")[1] : "")
+                    )).map((product, idx) => (
+                        <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                            <div className="mx-2"> 
+                                <Product product={product} />
+                            </div>
+                        </Col>
+                    )
+                    )}
+                </Row>
+            </div>
+        </div>
+    )
 }
 
 export default ProductsListPage
