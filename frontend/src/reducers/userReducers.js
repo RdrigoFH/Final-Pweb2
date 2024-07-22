fbcc2f526d4ced9b70e51d3fe0f0652a08c62eb2
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 19:37:43 -0500

    creando el reducer que maneja el estado del inicio de sesión del usuario

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -44,3 +44,29 @@ import {
     GET_ALL_ORDERS_SUCCESS,
     GET_ALL_ORDERS_FAIL,
 } from '../constants/index'
+
+export const userLoginReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_LOGIN_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case USER_LOGIN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                userInfo: action.payload
+            }
+        case USER_LOGIN_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case USER_LOGOUT:
+            return {}
+
+        default:
+            return state
+    }
+}
