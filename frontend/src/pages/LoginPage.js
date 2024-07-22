7f1b9ad384fe15572e7d6bda7889b27d672d6cad
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 19:56:22 -0500

    Agregando la renderizacion del componente que mostrara un error si hay un problema con el inicio de sesion ademas de un formulario y un registro

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -25,6 +25,56 @@ function LoginPage({ history }) {
         e.preventDefault()
         dispatch(login(username, password))
     }
+
+    return (
+        <div>
+            <Row className='justify-content-md-center'>
+                <Col xs={12} md={6}>                    
+                    <h1>Sign In</h1>                    
+                    {error && <Message variant='danger'>{error}</Message>}
+                    <Form onSubmit={submitHandler}>
+                        <Form.Group controlId='username'>
+                            <Form.Label>
+                                Username
+                    </Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="enter username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='password'>
+                            <Form.Label>
+                                Password
+                    </Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Button type="submit" variant='primary'>Sign In</Button>
+                    </Form>
+
+                    <Row className="py-3">
+                        <Col>
+                            Do not have an account?
+                    <Link
+                                to={`/register`}
+                            > Register</Link>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+
+    )
 }
 
 export default LoginPage
