4cb74b8a2e1e61bbdd5722e93b6cca1f50094245
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 20:04:11 -0500

    Renderizado del componente en el cual mostrara una tabla con los detalles de los pedidos los cuales podran ser filtrados

--- frontend/src/pages/OrdersListPage.js
+++ frontend/src/pages/OrdersListPage.js
@@ -70,6 +70,108 @@ function OrdersListPage() {
     const handleSearchTerm = (term) => {
         setCloneSearchTerm(term)
     };
+
+    return (
+        <div>
+            {loadingOrders && <span style={{ display: "flex" }}>
+                <h5>Getting Orders</h5>
+                <span className="ml-2">
+                    <Spinner animation="border" />
+                </span>
+            </span>}
+            {userInfo.admin && <SearchBarForOrdersPage handleSearchTerm={handleSearchTerm} placeholderValue={placeholderValue} />}
+                {orders.length > 0 ?
+                <Table className="mt-2" striped bordered>
+                    <thead>
+                        <tr className="p-3 bg-info text-white text-center">
+                            <th>Order Id</th>
+                            <th>Customer Name</th>
+                            <th>Card Used</th>
+                            <th>Delivery Address</th>
+                            <th>Ordered Item</th>
+                            <th>Paid Status</th>
+                            <th>Paid On</th>
+                            <th>Total Amount</th>
+                            <th>Delivered Status</th>
+                            <th>Delivered On</th>
+                            {userInfo.admin &&
+                                <th>Delivery Status</th>
+                            }
+                        </tr>
+                    </thead>
+
+                    {/* filter orders by name, address or ordered item */}
+
+                    {orders.filter((item) => (
+
+                        item.name.toLowerCase().includes(cloneSearchTerm)
+                        ||
+                        item.ordered_item.toLowerCase().includes(cloneSearchTerm)
+                        ||
+                        item.address.toLowerCase().includes(cloneSearchTerm)
+                    )
+
+                    ).map((order, idx) => (
+                        <tbody key={idx}>
+                            <tr className="text-center">
+                                <td>
+                                    {order.id}
+                                </td>
+                                <td>{order.name}</td>
+                                <td>{order.card_number}</td>
+                                <td>{order.address}</td>
+                                <td>{order.ordered_item}</td>
+                                <td>{order.paid_status ?
+                                    <i className="fas fa-check-circle text-success"></i>
+                                    :
+                                    <i className="fas fa-times-circle text-danger"></i>
+                                }</td>
+                                <td>{dateCheck(order.paid_at)}</td>
+                                <td>{order.total_price} INR</td>
+                                <td>{order.is_delivered ?
+                                    <i className="fas fa-check-circle text-success"></i>
+                                    :
+                                    <i className="fas fa-times-circle text-danger"></i>
+                                }</td>
+                                <td>{order.delivered_at}</td>
+                                {userInfo.admin &&
+                                    <td>
+                                        {order.is_delivered ?
+                                            <button
+                                                className="btn btn-outline-danger btn-sm"
+                                                onClick={() => changeDeliveryStatusHandler(order.id, false)}
+                                            >
+                                                {deliveryStatusChangeSpinner
+                                                    &&
+                                                    idOfchangeDeliveryStatus === order.id
+                                                    ?
+                                                    <Spinner animation="border" />
+                                                    :
+                                                    "Mark as Undelivered"}
+                                            </button>
+                                            :
+                                            <button
+                                                className="btn btn-outline-primary btn-sm"
+                                                onClick={() => changeDeliveryStatusHandler(order.id, true)}
+                                            >
+                                                {deliveryStatusChangeSpinner
+                                                    &&
+                                                    idOfchangeDeliveryStatus === order.id
+                                                    ?
+                                                    <Spinner animation="border" />
+                                                    :
+                                                    "Mark as delivered"}
+                                            </button>
+                                        }
+                                    </td>
+                                }
+                            </tr>
+                        </tbody>
+                    ))}
+                </Table>
+                : <Message variant="info">No orders yet.</Message> }
+        </div>
+    )
 }
 
 export default OrdersListPage
