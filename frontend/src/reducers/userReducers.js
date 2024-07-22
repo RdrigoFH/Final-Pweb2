109c6b67b65c5f6e26bbc6f0ec31af534c3349fa
miguelnodjan <malvarezcho@unsa.edu.pe>
2024-07-21 19:40:14 -0500

    añadiendo un reductor que maneja el estado del registro de un nuevo usuario

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -70,3 +70,29 @@ export const userLoginReducer = (state = {}, action) => {
             return state
     }
 }
+
+export const userRegisterReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_REGISTER_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case USER_REGISTER_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                userInfo: action.payload
+            }
+        case USER_REGISTER_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case USER_LOGOUT:
+            return {}
+
+        default:
+            return state
+    }
+}
