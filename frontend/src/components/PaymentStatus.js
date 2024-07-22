e9e2b0047d91ad2ffc2d14c2233fc5042a653924
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-07-21 22:40:31 -0500

    Se muestra el nombre, id e imagen de un producto y finalmente su precio

--- frontend/src/components/PaymentStatus.js
+++ frontend/src/components/PaymentStatus.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import { useLocation } from "react-router-dom";
-import { Link } from 'react-router-dom'
-import Message from "./Message"
+    import React from 'react'
+    import { Card } from 'react-bootstrap'
+    import { useLocation } from "react-router-dom";
+    import { Link } from 'react-router-dom'
+    import Message from "./Message"
 
-const PaymentStatus = () => {
-    const location = useLocation()
+    const PaymentStatus = () => {
+        const location = useLocation()
 
-    const renderData = () => {
+        const renderData = () => {
 
-        try {
-            const boughtData = location.state.detail
+            try {
+                const boughtData = location.state.detail
 
-            return (
-                <div>
-                    <h3 className="text-success">Payment was Successfull</h3>
-                    <Card className="p-3">
-                        Successfully bought
-                        <br />
-                        <span className="mb-2" style={{ display: "flex" }}>
-                            {boughtData.name},
-                            S/.{boughtData.price} <i className="text-primary ml-1 mt-1 fas fa-check-circle"></i>
-                        </span>
-                        <Link to="/all-orders/">Go to orders page</Link>
-                    </Card>
-                </div>
-            )
-        } catch (error) {
-            return <Message variant='info'>Payment status not available.</Message>
+                return (
+                    <div>
+                        <h3 className="text-success">Payment was Successfull</h3>
+                        <Card className="p-3">
+                            Successfully bought
+                            <br />
+                            <span className="mb-2" style={{ display: "flex" }}>
+                                {boughtData.name},
+                                S/.{boughtData.price} <i className="text-primary ml-1 mt-1 fas fa-check-circle"></i>
+                            </span>
+                            <Link to="/all-orders/">Go to orders page</Link>
+                        </Card>
+                    </div>
+                )
+            } catch (error) {
+                return <Message variant='info'>Payment status not available.</Message>
+            }
         }
+
+        return renderData()
     }
 
-    return renderData()
-}
-
-export default PaymentStatus
+    export default PaymentStatus
