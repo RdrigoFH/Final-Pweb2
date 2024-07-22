06d47402fb4a9035d173cbe3750e26ffeb336500
Eduardo Cuno <ecunos@unsa.edu.pe>
2024-07-21 20:26:03 -0500

    Agregando el renderizado del componente RegisterPage el cual mostrara un fomulario con los campos: nombre, correo, contraseña y confirmacion de contraseña

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -32,7 +32,87 @@ function RegisterPage({ history, variant }) {
         }
     }
 
-    
+    return (
+        <div>
+            <Row className='justify-content-md-center'>
+                <Col xs={12} md={6}>
+                    <h1>Sign Up</h1>
+                    {message && <Message variant='danger'>{message}</Message>}
+                    {error && <Message variant='danger'>{error}</Message>}
+                    <Form onSubmit={submitHandler}>
+
+                        <Form.Group controlId='name'>
+                            <Form.Label>
+                                Username
+                            </Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="enter your username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='email'>
+                            <Form.Label>
+                                Email Address
+                            </Form.Label>
+                            <Form.Control
+                                required
+                                type="email"
+                                placeholder="enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='password'>
+                            <Form.Label>
+                                Password
+                            </Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId='passwordConfirm'>
+                            <Form.Label>
+                                Confirm Password
+                            </Form.Label>
+                            <Form.Control
+                                required
+                                type="password"
+                                placeholder="confirm your password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            >
+                            </Form.Control>
+                        </Form.Group>
+
+                        <Button type="submit" variant='primary'>Sign Up</Button>
+                    </Form>
+
+                    <Row className="py-3">
+                        <Col>
+                            Already have an account?
+                    <Link
+                                to={`/login`}
+                            > Login</Link>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </div>
+
+    )
 }
 
 export default RegisterPage
